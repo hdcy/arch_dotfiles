af8b43c1809743f01db7d56456280cb1bbbdb5fc
hdcy <2621939606@qq.com>
2026-07-12 11:53:39 +0800

    动画速度修改

--- caelestia/hypr-user.lua
+++ caelestia/hypr-user.lua
@@ -80,14 +80,14 @@ end)
 hl.animation({
     leaf    = "workspaces",
     enabled = true,
-    speed   = 3,
+    speed   = 4,
     -- bezier  = "emphasizedDecel",
     bezier  = "standard",
     style   = "slidevert",
 })
 
 -- === 滚动聚焦动画 ===
-hl.animation({ leaf = "windowsMove", enabled = true, speed = 3, bezier  = "standard" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = 4, bezier  = "standard" })
 
 -- ============================================================
 --                      快捷键
